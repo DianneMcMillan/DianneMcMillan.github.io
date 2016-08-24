b47fec5fc3405b2aa2d79a5e502c3b4088c37f83
DianneMcMillan <boosmcmillan@gmail.com>
2016-08-23 20:09:23 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
 $("h1").fadeIn(1000);
 $("p").fadeIn(2000);
 $("header").click(function(){
-  $('header').css("background-color", "red");
+  $("header").css("background-color", "red");
 });
 $("#fancy-text").click(function(){
   $("#fancy-text").css({"font-style":"italic","color":"purple", "font-size": "30px"});
