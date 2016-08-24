4562efb0c66a7c4b6f5f77a1324323b01e1ec5f5
DianneMcMillan <boosmcmillan@gmail.com>
2016-08-23 20:03:18 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,9 +5,9 @@ $(document).ready(function(){
 $("h1").fadeIn(1000);
 $("p").fadeIn(2000);
 $("header").click(function(){
-  $(this).css("background-color", "red");
+  $('header').css("background-color", "red");
 });
 $("#fancy-text").click(function(){
-  $(this).css({"font-style":"italic","color":"purple", "font-size" "30px"});
+  $("fancy-text").css({"font-style":"italic","color":"purple", "font-size" "30px"});
 }):
 });
